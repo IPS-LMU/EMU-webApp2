d110b144eed4622a0055235970482719d98fb253
Raphael Winkelmann <raphael@phonetik.uni-muenchen.de>
2018-10-26 18:26:37 +0200

    uncomment to avoid unreachable code detection error

--- src/app/_services/draw-helper.service.ts
+++ src/app/_services/draw-helper.service.ts
@@ -448,10 +448,10 @@ export class DrawHelperService {
             ctx.stroke();
             ctx.fill();
             // if (ConfigProviderService.vals.restrictions.drawSampleNrs) {
-            if(false){
-              ctx.fillText(sNr.toString(), i / allPeakVals.samplePerPx - hDbS, canvas.height - (allPeakVals.samples[i] - allPeakVals.minSample) / (allPeakVals.maxSample - allPeakVals.minSample) * canvas.height - 10);
-              sNr = sNr + 1;
-            }
+            // if(false){
+            //   ctx.fillText(sNr.toString(), i / allPeakVals.samplePerPx - hDbS, canvas.height - (allPeakVals.samples[i] - allPeakVals.minSample) / (allPeakVals.maxSample - allPeakVals.minSample) * canvas.height - 10);
+            //   sNr = sNr + 1;
+            // }
           }
 
         }
